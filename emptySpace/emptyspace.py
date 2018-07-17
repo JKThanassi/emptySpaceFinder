dabf2abe159849d96b3056af8bde1267819aef23
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-07-17 13:54:38 -0400

    saving changes

--- emptySpace/emptyspace.py
+++ emptySpace/emptyspace.py
@@ -4,10 +4,11 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import distance
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.manifold import MDS
 from emptySpace.gabriel import Gabriel
 
 class Empty_Space(object):
-    def __init__(self, data, max_clusters):
+    def __init__(self, data, max_clusters, dim_to_scale):
         """constructor for Empty_Space object
         
         Args:
@@ -21,6 +22,7 @@ class Empty_Space(object):
         self.center_point_distances = list()
         self.ghost_points = list()
         self.max_clusters = max_clusters
+        self.dim_to_scale = dim_to_scale
     
     def find_empty_space(self):
         for temp_point in self.gabriel.point_graph:
@@ -38,6 +40,10 @@ class Empty_Space(object):
         scores = [silhouette_score(self.center_points, km.predict(self.center_points)) for km in km_list]
         best_km_idx = np.argmax(scores)
         self.ghost_points = km_list[best_km_idx].cluster_centers_
+
+
+    def scale(self):
+        mds = MDS(n_components=self.dim_to_scale)
 
 
     def plot(self):
