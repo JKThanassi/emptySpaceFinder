286d7d83f27ce3e0da677c1049669bf9c5140851
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-07-30 12:46:41 -0400

    added color coding to points to more easily recognize patterns in mds layouts

--- tests/make_plots.py
+++ tests/make_plots.py
@@ -6,9 +6,9 @@ import numpy as np
 data_dict = dict()
 
 for idx in range(5):
-    data_dict['normal' + str(idx)] = np.random.normal(size=(100, 2))
-    data_dict['gumbel' + str(idx)] = np.random.gumbel(size=(100, 2))
-    data_dict['logistic' + str(idx)] = np.random.logistic(size=(100, 2))
+    data_dict['normal' + str(idx)] = np.random.normal(size=(100, 3))
+    data_dict['gumbel' + str(idx)] = np.random.gumbel(size=(100, 3))
+    data_dict['logistic' + str(idx)] = np.random.logistic(size=(100, 3))
 
-make_plots = es_vs_no_es(data_dict, show_plots=False, save_plots=True)
+make_plots = es_vs_no_es(data_dict, show_plots=False, save_plots=True, save_path='plots/3d_scaled/')
 make_plots.generate_plots()
