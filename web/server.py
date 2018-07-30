e91b0387b8250c5e062a4b2befdef463dfaa0e94
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-07-30 15:56:55 -0400

    server now returns data and added boilerplate react code

--- web/server.py
+++ web/server.py
@@ -1,18 +1,19 @@
 import sys
 sys.path.append('../')
-from flask import Flask, jsonify, Request
+from flask import Flask, jsonify, request
 from emptySpace.emptyspace import Empty_Space
 import json
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/es/v1/find_es', methods=['POST'])
 def gen_empty_space():
-    if Request.is_json:
-        jsondata = Request.json
-        print(jsondata)
-        data = json.loads(jsondata)
-        es = Empty_Space(data['data'], data['clusters'], 2)
+    if request.is_json:
+        jsondata = request.get_json()
+        data = json.loads(jsondata['data'])
+        n_clusters = json.loads(jsondata['n_clusters'])
+        es = Empty_Space(np.asarray(data), n_clusters, 2)
         es.find_empty_space()
         scaled_data, scaled_centers = es.scale()
         toReturn = {'scaled_data':scaled_data, 'scaled_centers':scaled_centers}
@@ -20,5 +21,5 @@ def gen_empty_space():
 
 @app.route("/")
 def home():
-    return "nice"
+    return "Site to come"
 
