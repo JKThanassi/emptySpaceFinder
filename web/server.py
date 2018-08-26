aac34a2d4a2527224ba949641d120a0dbaa4742b
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-08-26 15:44:40 -0400

    saving changes

--- web/server.py
+++ web/server.py
@@ -9,11 +9,19 @@ app = Flask(__name__)
 
 @app.route('/es/v1/find_es', methods=['POST'])
 def gen_empty_space():
+    """This function handles api requests for to calculate emptyspace on the given datasets
+    
+    Returns:
+        JSON: The data--mds scaled to 2d--embedded with empty space points and the number of empty space points returned
+    """
+
     if request.is_json:
         jsondata = request.get_json()
+        # the raw csv-formatted data
         data = json.loads(jsondata['data'])
+        # the max number of clusters for k-means consider
         n_clusters = json.loads(jsondata['n_clusters'])
-        es = Empty_Space(np.asarray(data), n_clusters, 2)
+        es = Empty_Space(np.genfromtxt(data, delimiter=","), n_clusters, 2)
         es.find_empty_space()
         scaled_data, scaled_centers = es.scale()
         toReturn = {'scaled_data':es.toReactVisFormat(scaled_data), 'scaled_centers':es.toReactVisFormat(scaled_centers, es.ghost_point_avg_dist)}
@@ -21,5 +29,5 @@ def gen_empty_space():
 
 @app.route("/")
 def home(): 
-    return "Site to come"
+    return "This server is for the API you should not reach this page"
 
